74335e6a29134b13326a7798453388db1f83ed0c
NarendranathV13 <narendranath.v@arkinfotec.com>
2023-10-03 11:04:13 +0530

    api

--- src/api/ApiMethods.js
+++ src/api/ApiMethods.js
@@ -4,7 +4,7 @@ const api = axios.create({
 });
 // Adding authorization header to all requests
 api.interceptors.request.use((config) => {
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovLzEyaG91cm9ubGluZWFwaS5tZXRyb3RyYWZmaWNzY2hvb2wuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNjk2MjE5NzY0LCJleHAiOjE2OTYzMDYxNjQsIm5iZiI6MTY5NjIxOTc2NCwianRpIjoia0lhaEVlNkoyNEhvUkhIRSIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3IiwibmFtZSI6IkFkbWluIEFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20ifQ.pxchcvCdq5MVpwPZpeusSZVMULEYSHUT-Y9SyPVoVys'; 
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovLzEyaG91cm9ubGluZWFwaS5tZXRyb3RyYWZmaWNzY2hvb2wuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNjk2MzA2NzEyLCJleHAiOjE2OTYzOTMxMTIsIm5iZiI6MTY5NjMwNjcxMiwianRpIjoiano3YWhZTUlWQnF1QWFiRiIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3IiwibmFtZSI6IkFkbWluIEFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20ifQ.QGk25Pu_uMKrdwa5WXZ4WiXtNYylPHrWV9F0UkLcNF0'; 
     config.headers.Authorization = `Bearer ${token}`;
     return config;
   });
