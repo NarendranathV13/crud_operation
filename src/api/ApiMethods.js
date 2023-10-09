06a0f739f02c69504e934e044882840a14f59a5d
Perachi Kailasam <perachi.kailasam@ext.eyeball.club>
2023-10-09 14:42:02 +0530

    updated

--- src/api/ApiMethods.js
+++ src/api/ApiMethods.js
@@ -4,7 +4,7 @@ const api = axios.create({
 });
 // Adding authorization header to all requests
 api.interceptors.request.use((config) => {
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovLzEyaG91cm9ubGluZWFwaS5tZXRyb3RyYWZmaWNzY2hvb2wuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNjk2MzA2NzEyLCJleHAiOjE2OTYzOTMxMTIsIm5iZiI6MTY5NjMwNjcxMiwianRpIjoiano3YWhZTUlWQnF1QWFiRiIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3IiwibmFtZSI6IkFkbWluIEFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20ifQ.QGk25Pu_uMKrdwa5WXZ4WiXtNYylPHrWV9F0UkLcNF0'; 
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovLzEyaG91cm9ubGluZWFwaS5tZXRyb3RyYWZmaWNzY2hvb2wuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNjk2ODMwODcyLCJleHAiOjE2OTY5MTcyNzIsIm5iZiI6MTY5NjgzMDg3MiwianRpIjoibTdDZzl2SHBxU05MQmQ3VSIsInN1YiI6IjEiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3IiwibmFtZSI6IkFkbWluIEFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20ifQ.lhf0tSxQV4Y4iZ5GFOx9Yr30VAyvYimhYlwuhx1sXnY'; 
     config.headers.Authorization = `Bearer ${token}`;
     return config;
   });
